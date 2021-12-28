1fbed57f682a994b96ee3a427af4e65884550d3b
jxt1009 <36055740+jxt1009@users.noreply.github.com>
2021-12-28 00:02:37 -0500

    Fixed issue of overlapping points in canvasjs plot

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ import pymysql
 from flask import render_template
 import pandas as pd
 
-conn = pymysql.connect(host="db",
-					   port=3306,
+conn = pymysql.connect(host="10.0.0.147",
+					   port=3308,
 					   user="test",
 					   database="car_stats")
 
@@ -18,9 +18,9 @@ app = flask.Flask(__name__, static_url_path='',
 app.config["DEBUG"] = True
 
 # Global mapping variables
-voltage_graphs = pd.DataFrame()
 R1 = 8800
 R2 = 2200
+
 
 # Post route to submit voltage to SQL db
 @app.route('/voltage/<voltage>', methods=['GET', 'POST'])
@@ -40,13 +40,14 @@ def post_voltage(voltage):
 def get_voltage_chunks():
 	sql = "SELECT id,voltage,car_id,timestamp FROM voltage"
 	sql += " WHERE timestamp BETWEEN %s AND %s;"
-	params = ((datetime.utcnow()-timedelta(minutes=200)).strftime('%Y-%m-%dT%H:%M'),datetime.utcnow().strftime('%Y-%m-%dT%H:%M'),)
+	params = ((datetime.utcnow()-timedelta(minutes=60)).strftime('%Y-%m-%dT%H:%M'),
+			  datetime.utcnow().strftime('%Y-%m-%dT%H:%M'),)
 
 	db_results = pd.read_sql(sql=sql,con=conn,params=params)
-	db_results.voltage = db_results.voltage.apply(lambda x: x * ((R2+R1)/R2))
+	db_results['voltage'] = db_results.voltage.apply(lambda x: x * ((R2+R1)/R2))
 	db_results['voltage_avg'] = db_results.voltage.expanding().mean()
-	db_results = db_results.sort_values(by="timestamp")
 	return db_results
+
 
 # Display the homepage
 @app.route("/")
@@ -57,10 +58,7 @@ def homepage():
 @app.route("/voltage-chart")
 def get_voltage_chart():
 	chunks = get_voltage_chunks()
-	if len(chunks) > 0:
-		return chunks.to_json(orient="records")
-	else:
-		return None
+	return chunks.to_json(orient="records")
 
 
 if __name__ == '__main__':
