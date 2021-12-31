ff80964ebc7e7f41ded5ab1f1a088477f426e994
jxt1009 <36055740+jxt1009@users.noreply.github.com>
2021-12-31 13:23:38 -0500

    SQL connection now opens/closes for each call. Hoping to fix issues of a connection to db dropping and causing crashes

--- app/main.py
+++ app/main.py
@@ -4,12 +4,6 @@ import pymysql
 from flask import render_template
 import pandas as pd
 
-conn = pymysql.connect(host="10.0.0.147",
-					   port=3308,
-					   user="test",
-					   password="root",
-					   database="car_stats")
-
 app = flask.Flask(__name__, static_url_path='',
 				  static_folder='static',
 				  template_folder='template')
@@ -24,19 +18,29 @@ R2 = 2200
 # Post route to submit voltage to SQL db
 @app.route('/voltage/<voltage>', methods=['GET', 'POST'])
 def post_voltage(voltage):
+	conn = get_connection()
 	sql = "INSERT INTO voltage (voltage, car_id) VALUES (%s, %s)"
 	val = (voltage, "01")
 	cursor = conn.cursor()
 	cursor.execute(sql, val)
 	if conn.affected_rows() > 0:
 		conn.commit()
+		conn.close()
 		return "Success"
 	else:
+		conn.close()
 		return "Error"
 
+def get_connection():
+	return pymysql.connect(host="10.0.0.147",
+						   port=3308,
+						   user="test",
+						   password="root",
+						   database="car_stats")
 
 # Get the chunks of time series data from the SQL db
 def get_voltage_chunks(view_time):
+	conn = get_connection()
 	sql = "SELECT id,voltage,car_id,timestamp FROM voltage order by id desc limit %(view_time)s;"
 
 	if view_time is None:
@@ -47,6 +51,7 @@ def get_voltage_chunks(view_time):
 	db_results = pd.read_sql(sql=sql, con=conn, params={"view_time": view_time})
 	db_results['voltage'] = db_results.voltage.apply(lambda x: x * ((R2 + R1) / R2))
 	db_results['voltage_avg'] = db_results.voltage.expanding().mean()
+	conn.close()
 
 	return db_results
 
